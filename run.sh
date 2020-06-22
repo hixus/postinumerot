516a19b1ba0b5267dc56a1d3a78f6f67caa7ccc9
hixus <1814734+hixus@users.noreply.github.com>
2020-06-22 23:27:41 +0300

    use yesterday

--- run.sh
+++ run.sh
@@ -7,12 +7,16 @@ process_file() {
     local dt=$2
     local save_to=$3
     rm -f ${type}_${dt}.zip
-    wget ${BASE_PATH}/${type}_${dt}.zip
-    unzip -o ${type}_${dt}.zip
-    echo "Converting ${type}_${dt}.dat to ${save_to}"
-    cat ${type}_${dt}.dat | iconv -f iso-8859-1 -t utf-8 | node convert.js ${type} > ${save_to}
+    wget -q ${BASE_PATH}/${type}_${dt}.zip
+    if [ $? -ne 0 ]; then
+    echo "No ${type} update for ${dt}, skipping"
+    else
+         unzip -o ${type}_${dt}.zip
+        echo "Converting ${type}_${dt}.dat to ${save_to}"
+        cat ${type}_${dt}.dat | iconv -f iso-8859-1 -t utf-8 | node convert.js ${type} > ${save_to}
+    fi
 }
 
 
-process_file "PCF" "20200622" "Postinumerotiedosto.csv"
-process_file "BAF" "20200620" "Perusosoitteisto.csv"
+process_file "PCF" ${YESTERDAY} "Postinumerotiedosto.csv"
+process_file "BAF" ${YESTERDAY} "Perusosoitteisto.csv"
